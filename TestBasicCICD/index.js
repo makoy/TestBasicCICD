0b80f11749515b4493573cf750d8ae84e4e2580e
makoy <mack.mislang@gmail.com>
2021-11-22 21:48:15 +0800

    Update index.js
    pipe3

--- TestBasicCICD/index.js
+++ TestBasicCICD/index.js
@@ -3,8 +3,8 @@ module.exports = async function (context, req) {
 
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
-        ? "Hello, " + name + ". AMC-Pipe2.This HTTP triggered function executed successfully."
-        : "AMC-Pipe2.This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+        ? "Hello, " + name + ". AMC-Pipe3.This HTTP triggered function executed successfully."
+        : "AMC-Pipe3.This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.res = {
         // status: 200, /* Defaults to 200 */
