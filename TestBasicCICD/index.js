0947adcb97eface71ee1fa7f11b140e118a5e0e3
makoy <mack.mislang@gmail.com>
2023-08-15 23:06:57 +0800

    Update index.js
    version ver01

--- TestBasicCICD/index.js
+++ TestBasicCICD/index.js
@@ -2,9 +2,10 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const name = (req.query.name || (req.body && req.body.name));
+    const ver = "";
     const responseMessage = name
-        ? "Hello, " + name + ". AMC-Pipe3.This HTTP triggered function executed successfully."
-        : "AMC-Pipe3.This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+        ? "ver01 Hello, " + name + ". AMC-Pipe3.This HTTP triggered function executed successfully."
+        : "ver01 AMC-Pipe3.This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.res = {
         // status: 200, /* Defaults to 200 */
